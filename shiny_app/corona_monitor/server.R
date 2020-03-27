180be22d237a70eb0756170ca6657b8062c9c824
bv2 <britta.velten@gmail.com>
2020-03-27 12:09:08 +0100

    inital version of the App

---
+++ shiny_app/corona_monitor/server.R
@@ -0,0 +1,78 @@
+library(shiny)
+library(tidyverse)
+
+load("../../out/df_dt.RData")
+load("../../out/df.RData")
+
+shinyServer(function(input, output) {
+  
+  df_contrs <-  reactive({
+    df <- filter(df, country %in% input$cntrs, date > input$start_date)
+    if(input$ds100) {
+      df$x = df$daysAfter100Cases 
+      df <- filter(df, x >= 0)
+      } else df$x = df$date
+    df
+    })
+
+  xlab4plot <-   reactive({ifelse(input$ds100, "Days after 100 cases", "Date")})
+  df_dt_contrs <-  reactive({
+    df_dt <- filter(df_dt, country %in% input$cntrs, date > input$start_date)
+    if(input$ds100) {
+      df_dt$x = df_dt$date - df_dt$date0
+      df_dt$x_closure = df_dt$date_closure - df_dt$date0
+    } else {
+      df_dt$x = df_dt$date
+      df_dt$x_closure = df_dt$date_closure
+    }
+    df_dt
+    })
+  plotds100 <- reactive({input$ds100})
+  
+  output$cases <- renderPlot({
+    # generate an rnorm distribution and plot it
+    dftmp <- df_contrs()
+    xlab4plottmp <- xlab4plot()
+    gg <- dftmp %>% 
+      ggplot(aes(x=x, y = Confirmed, col = country)) +
+      geom_line() + ggtitle("Total cases") +xlab(xlab4plottmp) + theme_bw()
+    gg
+    
+  })
+  
+  output$deaths <- renderPlot({
+    # generate an rnorm distribution and plot it
+    dftmp <- df_contrs()
+    xlab4plottmp <- xlab4plot()
+    gg <- dftmp %>% 
+      ggplot(aes(x=x, y = Deaths, col = country)) +
+      geom_line() + ggtitle("Total cases") +xlab(xlab4plottmp)+ theme_bw()
+    gg
+  })
+  
+
+  output$death_rate <- renderPlot({
+    # generate an rnorm distribution and plot it
+    dftmp <- df_contrs()
+    xlab4plottmp <- xlab4plot()
+    gg <- dftmp %>% 
+      ggplot(aes(x=x, y = 100 * Deaths/Confirmed, col = country)) +
+      geom_line()+  ylab("Death rate (%)") + xlab(xlab4plottmp)+ theme_bw()
+   gg
+    
+  })
+  
+  output$doubling_time <- renderPlot({
+    # generate an rnorm distribution and plot it
+    dftmp <- df_dt_contrs()
+    xlab4plottmp <- xlab4plot()
+    gg <- ggplot(dftmp, aes(x=x, y = doubling_time, col = country)) +
+      geom_line() + ylab("doubling time (days)") +
+      geom_vline(aes(xintercept = x_closure, col = country), lty = "dashed") +
+      ggrepel::geom_text_repel(aes(x = x_closure, y= 5,
+                                   label = ifelse(!is.na(date_closure) & date == date_closure,  country, ""))) +
+      xlab(xlab4plottmp)+ theme_bw()
+   gg
+  })
+
+})
---
+++ shiny_app/corona_monitor/ui.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+
+load("../../out/df.RData")
+
+# Define UI for application that plots random distributions 
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Monitoring the development of Covid-19 (SARS-CoV-2)"),
+
+  # Sidebar with a slider input for number of observations
+  sidebarPanel(
+    sliderInput("start_date", 
+                "Starting date:", 
+                min = min(df$date),
+                max = max(df$date), 
+                value = as.Date("2020-01-01")),
+    checkboxInput("ds100",
+                  "Time since 100 cases", FALSE),
+    selectInput("cntrs", NULL , unique(df$country), selected = c("Germany", "Spain", "Italy", "UK"), multiple = TRUE,
+                selectize = TRUE, width = NULL, size = NULL)
+  ),
+
+  # Show a plot of the generated distribution
+  mainPanel(
+      tabsetPanel(
+                tabPanel("Cofirmed cases", 
+                        plotOutput("cases")
+                ),
+                tabPanel("Deaths",
+                        plotOutput("deaths")
+                ),
+                tabPanel("Death rates",
+                         plotOutput("death_rate")
+                ),
+                tabPanel("Doubling time & school closures",
+                         plotOutput("doubling_time")
+                )
+  )
+)))
